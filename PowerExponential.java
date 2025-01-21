bce9cfd49fb1ac561d060570fcccc837b488bcbe
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-01-21 19:33:27 +0530

    first commit

--- PowerExponential.java
+++ PowerExponential.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class PowerExponential {
-    public  static  void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        double number=sc.nextDouble();
-        double exponential= sc.nextDouble();
-        double powercalculated=Math.pow(number,exponential);
-        System.out.println(powercalculated);
+    public class PowerExponential {
+        public  static  void main(String [] args){
+            Scanner sc=new Scanner(System.in);
+            double number=sc.nextDouble();
+            double exponential= sc.nextDouble();
+            double powercalculated=Math.pow(number,exponential);
+            System.out.println(powercalculated);
+        }
     }
-}
