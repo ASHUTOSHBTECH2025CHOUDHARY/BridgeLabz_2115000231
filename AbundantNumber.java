e6cabbdb97ad3f2d3f628c2ddfff1b0b0a4f2440
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-01-23 23:53:36 +0530

    Level 3

--- AbundantNumber.java
+++ AbundantNumber.java
@@ -2,18 +2,30 @@ import java.util.Scanner;
 
 public class AbundantNumber {
     public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        int number= sc.nextInt();
-        int sum=0;
-        for(int i=0;i<number;i++){
-            if(number%i==0){
-                sum+=i;
+        // Create a Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //integer input from the user
+        int number = sc.nextInt();
+
+        // Initialize a variable the sum of the divisors of the number
+        int sum = 0;
+
+        // Loop through all integer to find divisors
+        for(int i = 1; i < number; i++) { // Changed `i=0` to `i=1` to prevent division by zero
+            // Check if 'i' is a divisor of 'number'
+            if(number % i == 0) {
+                // Add the divisor to the sum
+                sum += i;
             }
         }
-        if(sum>number){
+
+        // Check if the sum of the divisors is greater tahan the number itself
+        if(sum > number) {
+            // If true, the number is an abundant number
             System.out.println("It is a Abundant number");
-        }
-        else{
+        } else {
+            // Otherwise, the number is not an abundant number
             System.out.println("It is not a Abundant number");
         }
     }
