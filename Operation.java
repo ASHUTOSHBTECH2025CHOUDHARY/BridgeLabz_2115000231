9d6b7a411f192f1bd6c4b5b6aa223357273128fe
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-02-20 03:57:57 +0530

    [Ashutosh] Added SetDiffernce.java

--- Operation.java
+++ Operation.java
@@ -1,20 +1,20 @@
-import java.util.*;
+    import java.util.*;
 
-public class Operation {
-    public static void main(String[] args) {
-        // Initialize the first set
-        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+    public class Operation {
+        public static void main(String[] args) {
+            // Initialize the first set
+            Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
 
-        // Initialize the second set
-        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+            // Initialize the second set
+            Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
 
-        Set<Integer> unionSet = new HashSet<>(set1);
-        unionSet.addAll(set2);
-        System.out.println( unionSet);
+            Set<Integer> unionSet = new HashSet<>(set1);
+            unionSet.addAll(set2);
+            System.out.println( unionSet);
 
-        // Compute the intersection of the two sets
-        Set<Integer> intersectionSet = new HashSet<>(set1);
-        intersectionSet.retainAll(set2);
-        System.out.println(intersectionSet);
+            // Compute the intersection of the two sets
+            Set<Integer> intersectionSet = new HashSet<>(set1);
+            intersectionSet.retainAll(set2);
+            System.out.println(intersectionSet);
+        }
     }
-}
