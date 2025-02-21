9a13d241f72120c15ec5fcd5a11818f29cec84de
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-02-21 22:27:39 +0530

    [Ashutosh] Added DivisorCalculator.java

---
+++ DivisorCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DivisorCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try {
+            int dividend = input.nextInt();
+            int divisor = input.nextInt();
+            int quotient = dividend / divisor;
+            System.out.println(quotient);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed.");
+        } finally {
+            System.out.println("The operation has been completed.");
+        }
+    }
+}
