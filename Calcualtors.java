b47c8552738a52a248567cd7e957e577b9bad3b4
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-02-24 20:30:55 +0530

    [Ashutosh] Added Calcualtors.java

---
+++ Calcualtors.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Method;
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Create instance
+            Calculator calc = new Calculator();
+            // Get class object
+            Class<?> cls = calc.getClass();
+            // Get private method
+            Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+            // Allow access to private method
+            method.setAccessible(true);
+            int result = (int) method.invoke(calc, 5, 4);
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
